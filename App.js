3f82e95bc84a156d41afff20e1b320eebe836900
abdelrahmanayaad74 <abdelrahmanayad74@gmail.com>
2021-12-25 21:50:45 +0200

    modified App file

--- App.js
+++ App.js
@@ -7,10 +7,6 @@ class App extends Component {
       <View>
         <Text> Ayad git and github developer </Text>
         <Text> Abdelrahman Ayad git and github developer </Text>
-        <Text>Modify 2</Text>
-        <Text>Modify 3</Text>
-
-        <Text>Ady Modified 1</Text>
       </View>
     );
   }
