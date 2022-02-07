b93a67f1e2bd1ee2a742f3735dfc0dc716f1fbf8
abdelrahmanayaad74 <abdelrahmanayad74@gmail.com>
2022-02-07 16:11:49 +0200

    Ayad modify New Password page\

--- src/screens/auth/NewPassword.js
+++ src/screens/auth/NewPassword.js
@@ -4,6 +4,7 @@ import {
   View,
   StyleSheet,
   ImageBackground,
+  ScrollView,
 } from 'react-native';
 import {TextInput} from 'react-native-paper';
 
@@ -14,53 +15,107 @@ import GeneralButton from '../../components/GeneralButton';
 const {width, height} = Dimensions.get('screen');
 
 class NewPassword extends Component {
+  constructor() {
+    super();
+    this.state = {
+      newPass: '',
+      conNewPass: '',
+    };
+  }
   render() {
     return (
       <>
-        <View style={styles.container}>
-          <View style={styles.view1}>
-            <ImageBackground
-              source={require('../../assets/images/confirm.png')}
-              style={{
-                width: width * 1,
-                height: height * 0.3,
-              }}
-            />
-            <View style={styles.view2}>
-              <Text style={styles.text1}>انشاء رقم سري جديد ؟</Text>
-              <Text style={styles.text2}>
-                يجب ان يكون الرقم السري الجديد مختلف عن المستخدم سايقا
-              </Text>
-            </View>
-            <View style={styles.view3}>
-              <TextInput
-                style={styles.textInputStyle}
-                label={' الرقم السري الجديد '}
-                selectionColor="#ffcbb8"
-                underlineColor="#000"
-                activeUnderlineColor="#fb6e3b"
-                secureTextEntry
+        <ScrollView showsVerticalScrollIndicator={false} scrollEnabled={false}>
+          <View style={styles.container}>
+            <View style={styles.view1}>
+              <ImageBackground
+                source={require('../../assets/images/confirm.png')}
+                style={{
+                  width: width * 1,
+                  height: height * 0.3,
+                }}
               />
-              <TextInput
-                style={styles.textInputStyle}
-                label={' تأكيد الرقم السري '}
-                selectionColor="#ffcbb8"
-                underlineColor="#000"
-                activeUnderlineColor="#fb6e3b"
-                secureTextEntry
+              <View style={styles.view2}>
+                <Text style={styles.text1}>انشاء رقم سري جديد ؟</Text>
+                <Text style={styles.text2}>
+                  يجب ان يكون الرقم السري الجديد مختلف عن المستخدم سايقا
+                </Text>
+              </View>
+              <View style={styles.view3}>
+                <View>
+                  <TextInput
+                    value={this.state.newPass}
+                    onChangeText={value => {
+                      this.setState({newPass: value});
+                    }}
+                    style={styles.textInputStyle}
+                    label={' الرقم السري الجديد '}
+                    selectionColor="#ffcbb8"
+                    underlineColor="#000"
+                    activeUnderlineColor="#fb6e3b"
+                    secureTextEntry
+                  />
+                  {this.state.newPass.length == 0 ? null : (
+                    <Text
+                      style={{
+                        fontSize: 12,
+                        fontFamily: 'Tajawal',
+                        color: this.state.newPass.length < 8 ? 'red' : 'green',
+                      }}>
+                      {this.state.newPass.length < 8 ? (
+                        <Text>لابد ان يحتوي الرقم السري على الاقل 8 حروف</Text>
+                      ) : (
+                        <Text>الرقم السري يطابق الشروط</Text>
+                      )}
+                    </Text>
+                  )}
+                </View>
+                <View>
+                  <TextInput
+                    onChangeText={value => {
+                      this.setState({conNewPass: value});
+                    }}
+                    style={styles.textInputStyle}
+                    label={' تأكيد الرقم السري '}
+                    selectionColor="#ffcbb8"
+                    underlineColor="#000"
+                    activeUnderlineColor="#fb6e3b"
+                    secureTextEntry
+                  />
+                  {this.state.conNewPass.length == 0 ? null : this.state
+                      .conNewPass != this.state.newPass ? (
+                    <Text
+                      style={{
+                        fontSize: 12,
+                        fontFamily: 'Tajawal',
+                        color: 'red',
+                      }}>
+                      الرقم السري غير متطابق
+                    </Text>
+                  ) : (
+                    <Text
+                      style={{
+                        fontSize: 12,
+                        fontFamily: 'Tajawal',
+                        color: 'green',
+                      }}>
+                      الرقم السري متطابق
+                    </Text>
+                  )}
+                </View>
+              </View>
+              <GeneralButton
+                width={width * 0.5}
+                height={height * 0.07}
+                title="تأكيد"
+                BGcolor="#fb6e3b"
+                textColor="#ffffff"
+                textSize={SIZES.mediumFontSize}
+                haveBorder={false}
               />
             </View>
-            <GeneralButton
-              width={width * 0.5}
-              height={height * 0.07}
-              title="تأكيد"
-              BGcolor="#fb6e3b"
-              textColor="#ffffff"
-              textSize={SIZES.mediumFontSize}
-              haveBorder={false}
-            />
           </View>
-        </View>
+        </ScrollView>
       </>
     );
   }
@@ -69,7 +124,7 @@ class NewPassword extends Component {
 const styles = StyleSheet.create({
   container: {
     width: width,
-    height: height,
+    height: height * 0.95,
     alignItems: 'center',
   },
   view1: {
@@ -95,7 +150,7 @@ const styles = StyleSheet.create({
   },
   view3: {
     width: width * 0.9,
-    height: height * 0.2,
+    height: height * 0.21,
     backgroundColor: '#eee',
     justifyContent: 'space-around',
     alignItems: 'center',
