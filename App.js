1b868f26385d466057bf8c107161d550af5b638c
abdllrhmanelshazly <abdllrhmanelshazly@gmial.com>
2022-02-22 19:00:42 +0200

    Added OrdersScreen page

--- App.js
+++ App.js
@@ -1,19 +1,24 @@
-import React, {Component} from 'react';
-import {Text, View} from 'react-native';
-import LoginScreen from './src/screens/auth/LoginScreen';
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+// import LoginScreen from './src/screens/auth/LoginScreen';
 
-import SplashScreen from './src/screens/SplashScreen';
-import IntroSliderScreen from './src/screens/IntroSliderScreen';
+// import SplashScreen from './src/screens/SplashScreen';
+// import IntroSliderScreen from './src/screens/IntroSliderScreen';
+
+import Order from './src/screens/Order';
 
 // import ThemeProvider from './src/contexts/ThemeProvider';
 
-import ContainerNavigation from './src/navigation/containers/ContainerNavigation';
+// import ContainerNavigation from './src/navigation/containers/ContainerNavigation';
 
 class App extends Component {
   render() {
     return (
       <>
-        <ContainerNavigation></ContainerNavigation>
+        {/* <Cart /> */}
+        {/* <ContainerNavigation></ContainerNavigation> */}
+        {/* <Email /> */}
+        <Order />
       </>
     );
   }
